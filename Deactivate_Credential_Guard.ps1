3ab3f47ef08ed89ce99444b61de8885436246b6c
jsidler <joel.sidler@outlook.com>
2017-06-07 18:48:38 +0200

    Add files via upload

---
+++ Deactivate_Credential_Guard.ps1
@@ -0,0 +1,21 @@
+﻿#####################################################################################
+#### Author:                 Joël Sidler                                         ####
+#### Title:                  Deactivate Credential Guard                         ####
+#### Description:            PowerShell script for deactivating Credential Guard ####
+#### Creation date:          2017-02-07                                          ####
+#### Last modification date: 2017-02-07                                          ####
+#####################################################################################
+
+Start-Process powershell -verb runas
+
+Set-ExecutionPolicy unrestricted -Scope LocalMachine -Force
+
+$registryPath = "HKLM:\System\CurrentControlSet\Control\DeviceGuard"
+
+$Name = "EnableVirtualizationBasedSecurity"
+
+$value = "1"
+
+New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null
+
+pause
---
+++ Run_Script.ps1
@@ -0,0 +1,12 @@
+﻿#####################################################################################
+#### Author:                 Joël Sidler                                         ####
+#### Title:                  Deactivate Credential Guard                         ####
+#### Description:            PowerShell script for deactivating Credential Guard ####
+#### Creation date:          2017-02-07                                          ####
+#### Last modification date: 2017-02-07                                          ####
+#####################################################################################
+
+# Set-ExecutionPolicy to run all Windows PowerShell scripts
+powershell.exe -ExecutionPolicy Bypass -Scope LocalMachine -File .\Deactivate_Credential_Guard.ps1
+
+pause
